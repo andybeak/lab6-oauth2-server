a1267684b8c7eb693a9820e268e24fbd1ebb916c
Andrew Beak <andy.beak@gmail.com>
2019-12-04 17:11:58 +0000

    Pass request to router in public index

--- src/public/index.php
+++ src/public/index.php
@@ -11,3 +11,17 @@ $router = (new Router())
     ->setDispatcher($container['dispatcher'])
     ->setLogger($container['logger'])
     ->setHandler($container['controller']);
+
+try {
+    $routeInfo = $router->getRouteInfo();
+    $router->dispatchRoute($routeInfo);
+    http_response_code(200);
+} catch (RouteException | EncodingException $e) {
+    // we have sanitized the message so it's okay to display
+    echo $e->getMessage();
+    http_response_code(500);
+} catch (Exception $e) {
+    echo "Unexpected error";
+    $container['logger']->error("Unexpected error: {$e->getMessage()}");
+    http_response_code(500);
+}
