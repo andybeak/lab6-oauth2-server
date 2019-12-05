373edbde25f153a2f63187c452114db6ab5e877e
Andrew Beak <andy.beak@gmail.com>
2019-12-05 14:48:52 +0000

    Add ClientRepository

--- src/App/Bootstrap.php
+++ src/App/Bootstrap.php
@@ -34,6 +34,66 @@ class Bootstrap
                 ->setLogger($container['logger']);
         };
 
+        $container['ClientRepository'] = function($container) {
+            return (new ClientRepository())
+                ->setLogger($container['logger']);
+        };
+
+        $container['ScopeRepository'] = function($container) {
+            return (new ScopeRepository())
+                ->setLogger($container['logger']);
+        };
+
+        $container['AccessTokenRepository'] = function($container) {
+            return (new AccessTokenRepository())
+                ->setLogger($container['logger']);
+        };
+
+        $container['AuthCodeRepository'] = function($container) {
+            return (new AuthCodeRepository())
+                ->setLogger($container['logger']);
+        };
+
+        $container['RefreshTokenRepository'] = function($container) {
+            return (new RefreshTokenRepository())
+                ->setLogger($container['logger']);
+        };
+
+        $container['AuthorizationServer'] = function($container) {
+            // see https://oauth2.thephpleague.com/authorization-server/auth-code-grant/
+
+            $privateKey = 'file://' . __DIR__ . DIRECTORY_SEPERATOR . '..' . DIRECTORY_SEPERATOR . 'jwk/private.key';
+
+            // generate using base64_encode(random_bytes(32))
+            $encryptionKey = 'mwVGS+jB/41fBG6XLXug9VOXFV/sHc9jEaYxoQJUkK8=';
+
+            // Setup the authorization server
+            $server = new \League\OAuth2\Server\AuthorizationServer(
+                $container['clientRepository'],
+                $container['accessTokenRepository'],
+                $container['scopeRepository'],
+                $container['privateKey'],
+                $container['encryptionKey']
+            );
+
+            $grant = new \League\OAuth2\Server\Grant\AuthCodeGrant(
+                $container['authCodeRepository'],
+                $container['refreshTokenRepository'],
+                // authorization codes will expire after 10 minutes
+                new \DateInterval('PT10M')
+            );
+
+            // refresh tokens will expire after 1 month
+            $grant->setRefreshTokenTTL(new \DateInterval('P1M'));
+
+            // Enable the authentication code grant on the server
+            $server->enableGrantType(
+                $grant,
+                new \DateInterval('PT1H') // access tokens will expire after 1 hour
+            );
+
+        };
+
         return $container;
     }
 
---
+++ src/App/Repository/ClientRepository.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Repository;
+
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
+
+/**
+ * Class ClientRepository
+ * @package App\Repository
+ */
+class ClientRepository implements ClientRepositoryInterface
+{
+
+    /**
+     * We're hard coding this because we're only supporting our own client
+     * In a more complicated environment with multiple clients we'd use a
+     * database.
+     * @var string
+     */
+    private $clientSecret = '57087841224e';
+
+    /**
+     * @param string $clientIdentifier
+     * @return ClientEntityInterface
+     */
+    public function getClientEntity($clientIdentifier) : ClientEntityInterface
+    {
+
+        return new class($clientIdentifier) implements ClientEntityInterface {
+
+            private $clientIdentifier;
+
+            public function __construct($clientIdentifier)
+            {
+                $this->clientIdentifier = $clientIdentifier;
+            }
+
+            public function getIdentifier()
+            {
+                return $this->clientIdentifier;
+            }
+
+            public function getName()
+            {
+                return "Demo client";
+            }
+
+            public function getRedirectUri()
+            {
+                return "http://localhost/oauth2/callback";
+            }
+
+            public function isConfidential()
+            {
+                return false;
+            }
+        };
+    }
+
+    /**
+     * @param string $clientIdentifier
+     * @param string|null $clientSecret
+     * @param string|null $grantType
+     * @return bool
+     */
+    public function validateClient($clientIdentifier, $clientSecret, $grantType) : bool
+    {
+        return true;
+    }
+}
